10904da7406b71b3d032200338fa4b4c0fcf66b0
Farhad Hossain <farhadhossain0085@gmail.com>
2021-12-19 22:07:34 +0600

    alumni fields added

--- app_alumni/admin.py
+++ app_alumni/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
-from .models import Picture, Alumni
+from .models import Address, Picture, Alumni
+from django.utils.html import format_html
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['address', 'city', 'state', 'zip_code', 'created_at', 'updated_at']
 
 
 class AlumniAdmin(admin.ModelAdmin):
     list_display = [
         'name', 'session', 'passing_year', 'contact_number',
-        'email', 'is_employed', 'company', 'is_featured'
+        'email', 'is_employed', 'company', 'is_featured', 'image_tag'
     ]
     list_filter = ["session", "passing_year", "is_employed", "is_featured"]
     search_fields = ['name', 'contact_number', 'email']
+
+    def image_tag(self, obj):
+        if obj.profile_picture:
+            return format_html('<img src="{}" height="50px" />'.format(obj.profile_picture.picture.url))
+        return None
+    image_tag.short_description = 'Profile Picture'
+
 
 
 class PictureAdmin(admin.ModelAdmin):
@@ -17,3 +28,4 @@ class PictureAdmin(admin.ModelAdmin):
 
 admin.site.register(Picture, PictureAdmin)
 admin.site.register(Alumni, AlumniAdmin)
+admin.site.register(Address, AddressAdmin)
--- app_alumni/models.py
+++ app_alumni/models.py
@@ -4,7 +4,6 @@ from common.models import TrackingModel
 
 class Alumni(TrackingModel):
     name = models.CharField(max_length=200)
-    address = models.TextField(blank=True)
     birth_of_date = models.DateField(blank=True, null=True, help_text="Format: YYYY-MM-DD")
     session = models.CharField(max_length=10, help_text="Eg: 2012-13")
     passing_year = models.CharField(max_length=10)
@@ -16,7 +15,9 @@ class Alumni(TrackingModel):
     biography = models.TextField(blank=True)
     pictures = models.ManyToManyField('Picture', blank=True, related_name="alumnis")
     profile_picture = models.ForeignKey('Picture', related_name="dp_alumnis", on_delete=models.SET_NULL, blank=True, null=True)
-    is_featured = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False, help_text="Show in homepage or not.")
+    present_address = models.ForeignKey('Address', on_delete=models.SET_NULL, null=True, blank=True, related_name="present_address")
+    permanent_address = models.ForeignKey('Address', on_delete=models.SET_NULL, null=True, blank=True, related_name="permanent_address")
 
     def __str__(self) -> str:
         return self.name
@@ -28,3 +29,13 @@ class Picture(TrackingModel):
     def __str__(self) -> str:
         return self.picture.url
 
+
+class Address(TrackingModel):
+    address = models.TextField(blank=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    zip_code = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return str(self.id)
