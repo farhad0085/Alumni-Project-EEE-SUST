78641948120adfdfa84d1b736c15a31ab3f90e5c
Farhad Hossain <farhadhossain0085@gmail.com>
2025-09-28 01:51:23 +0600

    user model updated - removed username field, made email field primary

--- user/admin.py
+++ user/admin.py
@@ -5,15 +5,23 @@ from .models import UserAccount
 
 class UserAccountAdmin(UserAdmin):
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
         ('Permissions', {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ['email', 'first_name', 'last_name', 'id']
+    ordering = ['email',]
 
 
 admin.site.register(UserAccount, UserAccountAdmin)
--- user/models.py
+++ user/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from user.managers import UserManager
 
 
 class UserAccount(AbstractUser):
     """Custom user model for our system"""
 
+    username = None
+    email = models.EmailField('Email address', unique=True)
+    
+    def __str__(self):
+            return self.email
+    
     @property
     def full_name(self):
         name = self.get_full_name() or self.username
         return name
+    
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
